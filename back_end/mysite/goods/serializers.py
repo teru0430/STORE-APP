7a544b01b00b729b9d8f33bc1481e7f60cc051da
teru0430-windows <teruteruaraijudo@gmail.com>
2026-06-05 05:06:07 +0900

    APIと　商品のクラスを作成

---
+++ back_end/mysite/goods/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from  .models import Goods
+
+class GoodsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Goods
+        fields = '__all__'
---
+++ back_end/mysite/goods/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Goods
+from .serializers import GoodsSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+# Create your views here.
+class GoodsViewSet(viewsets.ModelViewSet):
+    queryset = Goods.objects.all()
+    serializer_class = GoodsSerializer
+    
+    
+@api_view(['GET'])
+def getAllGoods(request):
+    goods = Goods.objects.all()
+    
+    serializer = GoodsSerializer(goods, many=True)    
+    
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def createGoods(request):
+    data = request.data
+    
+    good = Goods.objects.create(
+        title = data['title'],
+        content = data['content'],
+        picture = data['picture'],
+    )
+@api_view(['PUT'])
+def updateGood(request, pk):
+    data = request.data
+    good = Goods.objects.get(id=pk)
+    
+    good.title = data['title']
+    good.content = data['content']
+    good.picture = data['picture']
+    good.public = data['public']
+    
+    good.save()
+    serializer = GoodsSerializer(good, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+
+def getGoodsById(request, pk):
+    good = get_object_or_404(Goods, id=pk)
+    try:
+        
+        serializer = GoodsSerializer(good, many=False)
+        return Response(serializer.data)
+    except:
+        return Response({"error": "error..."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+@api_view(['DELETE'])
+def deleteGood(request, pk):
+    good = Goods.objects.get(id=pk)
+    good.delete()
+    
+    return Response({'messege':'delete success'})
